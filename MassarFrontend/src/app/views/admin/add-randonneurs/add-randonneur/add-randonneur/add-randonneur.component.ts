9f3002c1b58d386b1bb4f65224cb640d032c555f
oumaima ghanmi <oumaima.ghanmi15@gmail.com>
2024-07-04 00:33:38 +0200

    update addRandonneur

--- MassarFrontend/src/app/views/admin/add-randonneurs/add-randonneur/add-randonneur/add-randonneur.component.ts
+++ MassarFrontend/src/app/views/admin/add-randonneurs/add-randonneur/add-randonneur/add-randonneur.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { NgForm, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
-import { HttpClient, HttpEventType, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpEventType, HttpResponse } from '@angular/common/http';
 import { Randonneur } from 'src/app/models/randonneur';
 import { RandonneurServService } from 'src/app/services/randonneur-serv.service';
 import { FileUploadServiceService } from 'src/app/services/file-upload-service.service';
@@ -38,28 +38,13 @@ ngOnInit(): void {
   })
   this.randonneurService.getRandonneur().subscribe(data=>{
   
-    this.dataArray=data
+    this.dataRandonneur=data
     
     
   })
 }
-sendEmail(form: NgForm) {
-  const to = form.value.to;
-  const cc = form.value.cc ? [form.value.cc] : [];
-  const subject = form.value.subject;
-  const body = form.value.body;
-  // hnee hawil dima hot les atrebut li chtab3ath'hom lil back fard objet kima l email w e9bilhom b request body
-  const email = {
-    to: to,
-    cc: cc,
-    subject: subject,
-    body: body
-  }
-  this.http.post('http://localhost:8080/auth/send', email).subscribe(
-      (response: any) => console.log("response")
-    );
-  form.resetForm();
-}
+
+  
 add() {
   let data = this.loginForm.value;
 
@@ -71,9 +56,9 @@ add() {
 
   console.log(data);
 
-  this.ds.addUser(data).subscribe(
+  this.randonneurService.addRandonneur(data).subscribe(
     response => {
-      this.refreshAssociationsData();
+      this.refreshRandonneurData();
 this.ngOnInit();
     },
     (err: HttpErrorResponse) => {
@@ -82,37 +67,37 @@ this.ngOnInit();
   );
 }
 
-onDeleteUser(idUserToDelete:number){
-this.idUserDeleteConfirm=idUserToDelete
+onDeleteRandonneur(idRandonneurToDelete:number){
+this.idRandonneurDeleteConfirm=idRandonneurToDelete
 
 }
 ConfirmDelete(){
-this.ds.deleteUser( this.idUserDeleteConfirm).subscribe((deleteUser)=>(console.log("besoin deleted"))
+this.randonneurService.deleteRandonneur( this.idRandonneurDeleteConfirm).subscribe((deleteRandonneur)=>(console.log("Randonneur deleted"))
 )
-this.ds.getAssociations().subscribe(data=>{
+this.randonneurService.getRandonneur().subscribe(data=>{
 
 this.dataArray=data
 this.refreshAssociationsData();
 this.ngOnInit();
 })
 this.reloadData();
-this.router.navigate(["/admin/association"])
+this.router.navigate(["/admin/gestionrandonneurs"])
 
 }
 reloadData() {
 throw new Error('Method not implemented.');
 }
-ConfirmDeleteBesoin(idBesoin:number){
+ConfirmDeleteRandonneur(idRandonneur:number){
 
 }
 details(idUser:any){
 
 
 this.user= new RegisterRequest()
-this.ds.getUserById(idUser).subscribe(data=>{
+this.randonneurService.getRandonneurById(idRandonneur).subscribe(data=>{
   console.log(data)
   this.user=data
-  console.log(idUser)
+  console.log(idRandonneur)
   
 })
 }
