d7bcf5ec12146a1394fbc7dcfc19882bfe43e1b6
oumaima ghanmi <oumaima.ghanmi15@gmail.com>
2024-07-05 03:18:10 +0200

    second commit

--- MassarFrontend/src/app/views/front/login/login/login.component.ts
+++ MassarFrontend/src/app/views/front/login/login/login.component.ts
@@ -1,66 +1,80 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
-import {  AuthServiceService } from 'src/app/services/auth-service.service';
+import { AuthServiceService } from 'src/app/services/auth-service.service';
 import { AuthResponseData } from 'src/app/models/auth-response-data';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit , OnDestroy {
-  errorMessage!: string; // Variable to store error messages
-  AuthUserSub!: Subscription; // Subscription to the authenticated user observable
+export class LoginComponent implements OnInit, OnDestroy {
+  errorMessage: string = ''; // Initialize errorMessage
 
- // Inject AuthService and Router in the constructor
- constructor(private authService: AuthServiceService, private router: Router) { }
+  AuthUserSub: Subscription | undefined; // Initialize AuthUserSub as Subscription or undefined
 
- // Lifecycle hook that is called after Angular has initialized all data-bound properties
- ngOnInit() {
-   
-   this.AuthUserSub = this.authService.AuthenticatedUser$.subscribe({
-     next: user => {
-       
-       if (user) {
-         this.router.navigate(['']);
-       }
-     }
-   });
- }
+  constructor(private authService: AuthServiceService, private router: Router) { }
 
- // Method to handle the sign-in form submission
- onSubmitSingin(formLogin: NgForm) {
-   // Validate the form
-   if (!formLogin.valid) {
-     return;
-   }
+  ngOnInit() {
+    // Auto-login if there's a saved user
+    this.authService.autoLogin();
 
-   const email = formLogin.value.email; // Get email from the form
-   const password = formLogin.value.password; // Get password from the form
-
-   // Call the login method from AuthService
-   this.authService.login(email, password).subscribe({
-    next: (authResponse: AuthResponseData) => {
-      // Check if 'participant' role exists in roles array
-      if (authResponse.roles.includes('Participant')) {
-        this.router.navigate(['/admin']);
-      } else {
-        this.router.navigate(['/']);
+    // Subscribe to the AuthenticatedUser$ observable to monitor authentication state
+    this.AuthUserSub = this.authService.AuthenticatedUser$.subscribe({
+      next: user => {
+        // If a user is authenticated, navigate based on their role
+        if (user) {
+          this.navigateBasedOnRole(user);
+        }
       }
-    },
-    error: err => {
-      this.errorMessage = err;
-    }
-  });
-  
- }
+    });
+  }
 
- // Lifecycle hook that is called when the component is destroyed
- ngOnDestroy() {
-   // Unsubscribe from the AuthenticatedUser$ observable to prevent memory leaks
-   this.AuthUserSub.unsubscribe();
- }
+  // Method to handle the sign-in form submission
+  onSubmitSingin(formLogin: NgForm) {
+    // Validate the form
+    if (!formLogin.valid) {
+      return;
+    }
+    const email = formLogin.value.email; // Get email from the form
+    const password = formLogin.value.password; // Get password from the form
+
+    // Call the login method from authService
+    this.authService.login(email, password).subscribe(
+      (userData: AuthResponseData) => {
+        if (userData) {
+          this.navigateBasedOnRole(userData); // Navigate based on the user's role
+        } else {
+          this.router.navigate(['/']); // Navigate to the default route if user data is not available
+        }
+      },
+      (error: any) => {
+        // Handle login errors
+        this.errorMessage = error.message;
+        this.router.navigate(['/']); // Navigate to the default route on error
+      }
+    );
+  }
+
+  navigateBasedOnRole(user: any /* ajustez le type de user en fonction de votre modèle User */) {
+    if (user.role === 'ROLE_Admin') {
+      this.router.navigate(['/admin']);
+    } else if (user.role === 'ROLE_Participant') {
+      this.router.navigate(['/participant']);
+    } else if (user.role === 'ROLE_Randonneur') {
+      this.router.navigate(['/randonneur']);
+    } else {
+      this.router.navigate(['/']); // Naviguer vers la route par défaut si le rôle est inconnu
+    }
+  }
+
+  // Lifecycle hook that is called when the component is destroyed
+  ngOnDestroy() {
+    // Unsubscribe from the AuthenticatedUser$ observable to prevent memory leaks
+    if (this.AuthUserSub) {
+      this.AuthUserSub.unsubscribe();
+    }
+  }
 }
- 
